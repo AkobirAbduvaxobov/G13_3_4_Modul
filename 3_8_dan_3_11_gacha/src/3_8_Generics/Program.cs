2c9f545940dd196362689c89947a4ee1b4feda6c
Akobir <akobirabduvahobov@gmail.com>
2026-02-26 17:40:57 +0500

    added

---
+++ 3_8_dan_3_11_gacha/src/3_8_Generics/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+
+namespace _3_8_Generics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //var box = new Box();
+        //box.Value = 45;
+        //box.Value = "salom";
+       
+        //var box = new MyBox<int>();
+        //box.Value = 45;
+        //Console.WriteLine(box.GetValue());
+
+        //var box2 = new MyBox<string>();
+        //box2.Value = "salom";
+        //Console.WriteLine(box2.GetValue());
+
+        //Box box = new Box();
+        //Console.WriteLine(box.GetRes(5));
+        //Console.WriteLine(box.GetRes("salom"));
+        //Console.WriteLine(box.GetRes('e'));
+
+
+        MyList<int> myList = new MyList<int>();
+
+        myList.Add(43);
+        myList.Add(45);
+        myList.Add(11);
+
+        Console.WriteLine(myList.Count);
+        Console.WriteLine(myList.Capacity);
+
+
+
+    }
+}
+
+
+public class Box
+{
+    public object Value { get; set; }
+
+    public T GetRes<T>(T t)
+    {
+        if (t is int intValue)
+        {
+            intValue += 10;
+            t = (T)(object)intValue;
+        }
+
+        else if(t is string s)
+        {
+            s += "10";
+            t = (T)(object)s;
+        }
+
+        return t;
+    }
+}
+
+public class MyBox<T>
+{
+    public MyBox()
+    {
+        
+    }
+
+    public T Value { get; set; }
+
+    public T GetValue()
+    {
+        if (Value is int intValue)
+        {
+            intValue += 10;
+            Value = (T)(object)intValue; 
+        }
+
+        return Value;   
+    }
+}
