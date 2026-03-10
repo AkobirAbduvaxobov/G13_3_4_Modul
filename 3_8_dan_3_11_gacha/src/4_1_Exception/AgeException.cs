ff8fe1e79dcbcdf7ff2d8d278a38d3eee49be06f
Akobir <akobirabduvahobov@gmail.com>
2026-03-10 16:22:42 +0500

    ADDEE

---
+++ 3_8_dan_3_11_gacha/src/4_1_Exception/AgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_1_Exception
+{
+    public class AgeException : Exception
+    {
+        public AgeException(string errorMessage) : base(errorMessage)
+        { }
+    }
+}
---
+++ 3_8_dan_3_11_gacha/src/4_1_Exception/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace _4_1_Exception;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        try
+        {
+            int age = 17;
+            if(age < 18)
+            {
+                throw new AgeException("Yosh 18 dan kichik bo'lishi mumkin emas!");
+            }
+        }
+        catch (AgeException ex)
+        {
+            Console.WriteLine("Hatolik");
+        }
+
+
+        //while (true)
+        //{
+        //    Do1();
+
+        //    Console.Write("Son kiriting : ");
+
+        //    int res;
+
+
+        //    try
+        //    {
+        //        int num = int.Parse(Console.ReadLine());
+        //        //res = 25 / num;
+        //        //Console.WriteLine("Res : " + res);
+        //        throw new StackOverflowException("Stack to'ldi");
+        //    }
+        //    catch (DivideByZeroException ex)
+        //    {
+        //        Console.WriteLine("Cannot divide by zero!");
+        //        Console.WriteLine(ex.Message);
+        //        res = 0;
+        //    }
+        //    catch (FormatException ex)
+        //    {
+        //        Console.WriteLine("Format xato!");
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Xatolik yuz berdi!");
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        Console.WriteLine("Bu har doim ishlaydi!");
+        //    }
+
+        //}
+    }
+
+    static void Do1()
+    {
+        try
+        {
+            Do2();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    static void Do2()
+    {
+        Do3();
+    }
+    static void Do3()
+    {
+        Do4();
+    }
+
+    static void Do4()
+    {
+        int a = 0;
+        Console.WriteLine(45 / a);
+    }
+
+
+}
