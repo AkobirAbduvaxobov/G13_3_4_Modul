74c61bbeea3b08b1a2185b579d7b0d34dda7c900
Akobir <akobirabduvahobov@gmail.com>
2026-03-12 16:54:38 +0500

    added

--- 4_2_dan_4_11_gacha/src/4_2_dars_Thread/Program.cs
+++ 4_2_dan_4_11_gacha/src/4_2_dars_Thread/Program.cs
@@ -6,11 +6,13 @@ internal class Program
     static string path = "D:\\Work\\Groups\\G_13\\Moduls\\G13_3_4_Modul\\4_2_dan_4_11_gacha\\src\\4_2_dars_Thread\\g13\\";
     static void Main(string[] args)
     {
-        for (int i = 10; i <= 19; i++)
-        {
-            Thread thread = new Thread(Display);
-            thread.Start(i);
-        }
+        
+
+        //for (int i = 10; i <= 19; i++)
+        //{
+        //    Thread thread = new Thread(Display);
+        //    thread.Start(i);
+        //}
     }
     static void Display(object num)
     {
