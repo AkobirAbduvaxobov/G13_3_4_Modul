df68f2adbb982ee3b1211a0112062e77a433b169
Akobir <akobirabduvahobov@gmail.com>
2026-03-12 15:58:11 +0500

    added 4.2 dars

---
+++ 4_2_dan_4_11_gacha/src/4_2_dars_Thread/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _4_2_dars_Thread;
+
+internal class Program
+{
+    static object locker = new object();    
+    static string path = "D:\\Work\\Groups\\G_13\\Moduls\\G13_3_4_Modul\\4_2_dan_4_11_gacha\\src\\4_2_dars_Thread\\g13\\";
+    static void Main(string[] args)
+    {
+        for (int i = 10; i <= 19; i++)
+        {
+            Thread thread = new Thread(Display);
+            thread.Start(i);
+        }
+    }
+    static void Display(object num)
+    {
+        
+        int number = (int)num;
+        for (int i = 0; i < number; i++)
+        {
+            lock(locker)
+            {
+                string newPath = Path.Combine(path, $"{i}_{number}");
+                Directory.CreateDirectory(newPath);
+            }
+
+        }
+    }
+}
