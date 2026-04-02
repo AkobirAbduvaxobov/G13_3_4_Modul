fd499f335d3d9c34e5b2918f19dea3f00b3903b6
Akobir <akobirabduvahobov@gmail.com>
2026-04-02 17:26:24 +0500

    added 4.10

---
+++ OnlineWebManagement/src/OnlineWebManagement.Api/Controllers/FolderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineWebManagement.Services.Services;
+
+namespace OnlineWebManagement.Api.Controllers;
+
+[Route("api/folders")]
+[ApiController]
+public class FolderController : ControllerBase
+{
+    private readonly IStoragerService StoragerService;
+    public FolderController()
+    {
+        StoragerService = new LocalStoragerService();
+    }
+
+    [HttpPost()]
+    public void AddFolder(string folderPath)
+    {
+        StoragerService.CreateFolder(folderPath);
+    }
+
+    [HttpDelete()]
+    public void DeleteFolder(string folderPath)
+    {
+        StoragerService.DeleteFolder(folderPath);
+    }
+}
---
+++ OnlineWebManagement/src/OnlineWebManagement.Services/Services/IStoragerService.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineWebManagement.Services.Services;
+
+public interface IStoragerService
+{
+    public void CreateFolder(string folderPath);
+    public void DeleteFolder(string folderPath);
+}
